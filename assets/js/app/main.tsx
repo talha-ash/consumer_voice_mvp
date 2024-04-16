aead6184ed7fcbbfe2fc3c9c8ef45158c7120419
talha-ash <ta.xit@yandex.com>
2024-04-16 22:07:14 +0500

    add migration and update register by adding more fields

--- assets/js/app/main.tsx
+++ assets/js/app/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
-
+import "./user_socket";
 const rootElement = document.getElementById("root");
 
 if (rootElement) {
---
+++ assets/types.d.ts
@@ -0,0 +1,4 @@
+// assert that an import module of this path HAS types
+declare interface Window {
+  userToken: string;
+}
